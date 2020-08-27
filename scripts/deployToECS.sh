4efa0b7a9e740af1f2d2876beb6bbdfd8755a7de
pankaj0590 <pankaj0590@gmail.com>
2020-08-27 22:08:04 +0530

    Update deployToECS.sh

--- scripts/deployToECS.sh
+++ scripts/deployToECS.sh
@@ -6,8 +6,8 @@
 dockerRepo=`aws ecr describe-repositories --repository-name dev-spring-boot-hello-world --region us-east-1 | grep repositoryUri | cut -d "\"" -f 4`
 dockerTag=`aws ecr list-images --repository-name dev-spring-boot-hello-world --region us-east-1 | grep imageTag | head -n 1 | cut -d "\"" -f 4`
 sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" scripts/template.json > taskDefinition.json
-aws ecs create-cluster --cluster-name ec2
+aws ecs create-cluster --cluster-name FARGATE-CLUSTER
 aws ecs register-task-definition --family spring-boot-hello-world --cli-input-json file://taskDefinition.json --region us-east-1
-aws ecs create-service --cluster ec2 --service-name spring-boot-hello-world-service --task-definition spring-boot-hello-world --desired-count 1
+aws ecs create-service --cluster FARGATE-CLUSTER --service-name spring-boot-hello-world-service --task-definition spring-boot-hello-world --desired-count 1
 revision=`aws ecs describe-task-definition --task-definition spring-boot-hello-world --region us-east-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
-aws ecs update-service --cluster ec2 --service spring-boot-hello-world-service --task-definition spring-boot-hello-world:${revision} --desired-count 1
+aws ecs update-service --cluster FARGATE-CLUSTER --service spring-boot-hello-world-service --task-definition spring-boot-hello-world:${revision} --desired-count 1
