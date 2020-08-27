78fa1dca4f92ff3ad26f6e10165f0e9c9e59665e
pankaj0590 <pankaj0590@gmail.com>
2020-08-27 20:56:49 +0530

    Update deployToECS.sh

--- scripts/deployToECS.sh
+++ scripts/deployToECS.sh
@@ -8,6 +8,6 @@ dockerTag=`aws ecr list-images --repository-name dev-spring-boot-hello-world --r
 sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" scripts/template.json > taskDefinition.json
 aws ecs create-cluster --cluster-name aws-ec2-cluster
 aws ecs register-task-definition --family spring-boot-hello-world --cli-input-json file://taskDefinition.json --region us-east-1
-aws ecs create-service --cluster aws-ec2-cluster --service-name spring-boot-hello-world-service --task-definition spring-boot-hello-world-task --desired-count 1
-revision=`aws ecs describe-task-definition --task-definition spring-boot-hello-world-task --region us-east-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
-aws ecs update-service --cluster aws-ec2-cluster --service spring-boot-hello-world-service --task-definition spring-boot-hello-world-task:${revision} --desired-count 1
+aws ecs create-service --cluster aws-ec2-cluster --service-name spring-boot-hello-world-service --task-definition spring-boot-hello-world --desired-count 1
+revision=`aws ecs describe-task-definition --task-definition spring-boot-hello-world --region us-east-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
+aws ecs update-service --cluster aws-ec2-cluster --service spring-boot-hello-world-service --task-definition spring-boot-hello-world:${revision} --desired-count 1
