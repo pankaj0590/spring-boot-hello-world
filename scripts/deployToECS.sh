4a653a981a60d7df2d99c6ae04195d8c001a8901
pankaj0590 <pankaj0590@gmail.com>
2020-08-27 18:07:03 +0530

    Update deployToECS.sh

--- scripts/deployToECS.sh
+++ scripts/deployToECS.sh
@@ -9,4 +9,4 @@ sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" scripts/template.json > taskDefi
 aws ecs describe-clusters --cluster aws-ec2-cluster --region us-east-1
 aws ecs register-task-definition --family jenkins-test --cli-input-json file://taskDefinition.json --region us-east-1
 revision=`aws ecs describe-task-definition --task-definition jenkins-test --region us-east-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
-aws ecs update-service --cluster test-cluster --service test-service --task-definition jenkins-test:${revision} --desired-count 1
+aws ecs update-service --cluster aws-ec2-cluster --service test-service --task-definition jenkins-test:${revision} --desired-count 1
